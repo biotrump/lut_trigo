d9da37a69c7fd3feb593564f50562e6f37e13135
thomas tsai <thomas@biotrump.com>
2015-06-12 09:24:22 +0800

    1. rename file name of trigonometry lookup table from f90.f90 -> trigo_lut.f90 2. test_lt.f90 -> test_lt.F90 : ".F90" can use conditional compiling by gcc's preprocessor.

--- build.sh
+++ build.sh
@@ -14,15 +14,18 @@ fi
 
 #the f90.f90 is the sine,cosine lookup table!
 if [ -f bin/c2f_lut ]; then
-	./bin/c2f_lut > f90.f90
-	cat ../lut.f90 >> f90.f90
-	echo "building f90.f90"
-	gfortran -o test_lut  ../test_lt.f90 f90.f90
+	./bin/c2f_lut > trigo_lut.f90
+	cat ../lut.f90 >> trigo_lut.f90
+	echo "building trigo_lut.f90"
+	gfortran -o test_lut  ../test_lt.F90 trigo_lut.f90
 	if [ $? != 0 ]; then
 		echo "make test_lut error"
 		exit -1
 	fi
 	echo "running sine, cosine lut test"
 	./test_lut
+	if [ $? == 0 ]; then
+		cp trigo_lut.f90 ..
+	fi
 fi
 popd
